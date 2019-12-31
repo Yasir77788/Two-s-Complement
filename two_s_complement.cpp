f869717423eab218c53f0e74a88fa5bf4fdbdeb7
Yasir_H <45829991+Yasir77788@users.noreply.github.com>
2019-12-31 00:11:57 -0600

    Add files via upload

---
+++ two_s_complement.cpp
@@ -0,0 +1,38 @@
+// Two's complement of an integer
+
+//  Output integer and its 1's complement in decimal and hexadecimal.
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	int value{};
+	std::cout << "Enter any integer: ";
+	std::cin >> value;
+	int inverted_value{ ~value };
+
+	unsigned int hex_digits{ 2 * sizeof(int) };          // Hex digits in value
+	unsigned int hex_value_width{ hex_digits + 2 };      // Add 2 for 0x prefix
+	unsigned int column_width{ hex_value_width + 4 };    // Output column width (add 4 for padding)
+
+	// Output column headings
+	std::cout << std::right << std::setw(column_width) << "value"
+		<< std::setw(column_width) << "~value"
+		<< std::setw(column_width) << "~value+1" << std::endl;
+
+	// Output hexadecimal values
+	std::cout << std::hex << std::showbase << std::internal << std::setfill('0')
+		<< "    " << std::setw(hex_value_width) << value
+		<< "    " << std::setw(hex_value_width) << inverted_value
+		<< "    " << std::setw(hex_value_width) << inverted_value + 1 << std::endl;
+
+	// Output decimal values
+	std::cout << std::dec << std::right << std::setfill(' ')
+		<< std::setw(column_width) << value
+		<< std::setw(column_width) << inverted_value
+		<< std::setw(column_width) << inverted_value + 1 << std::endl;
+
+	std::cin.get();
+	return 0;
+}
